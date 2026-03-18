7ed34ba1f3d797726307bb9e9644157594737c92
discobeast <javanwyk@waimea.school.nz>
2026-03-19 12:11:40 +1300

    Documentation

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -4,13 +4,14 @@ fun main() {
     val terminal = TerminalBuilder.builder().build()
     terminal.enterRawMode() // Disables line buffering
     val reader = terminal.reader()
-
     println("Press any key (Press 'q' to quit)...")
+    val charbuffer = MutableList(2) { 'a' }
     while (true) {
         val charCode = reader.read()
         val char = charCode.toChar()
+        charbuffer.add(0, char)
+        if (charbuffer.size > 2) charbuffer.removeLast()
         println("Key detected: $char (Code: $charCode)")
-        if (char == 'q') break
+        println("Last two keys detected: $charbuffer")
     }
-    terminal.close()
 }
