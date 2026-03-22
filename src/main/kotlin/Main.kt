43671a528f643020acbf50ee1b62319bb483758b
discobeast <javanwyk@waimea.school.nz>
2026-03-23 10:51:16 +1300

    Added wait function to main to allow detection of desired keypresses and to act apon them

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,39 @@
 import org.jline.terminal.TerminalBuilder
+import org.jline.utils.NonBlockingReader
+
+fun waitforkey(reader: NonBlockingReader): Int {
+    var char: Int = -1
+    var numVals = 1
+    var cursorKey = false
+
+    while (numVals > 0) {
+        val charCode = reader.read()
+        numVals--
+        when (charCode) {
+            27 -> { // Cursor keys are 3 bytes, first is 27 (ESC)
+                numVals += 2
+                cursorKey = true
+            }
+        }
+        char = charCode
+    }
+
+    return if (cursorKey) {
+        when (char) {
+            65 -> 1
+            66 -> 2
+            67 -> 3
+            68 -> 4
+            else -> -1
+        }
+    } else {
+        char
+    }
+}
 
 fun main() {
     val terminal = TerminalBuilder.builder().build()
     terminal.enterRawMode() // Disables line buffering
     val reader = terminal.reader()
-    println("Press any key (Press 'q' to quit)...")
-    val charbuffer = MutableList(3) { 0 }
-    while (true) {
-        val charCode = reader.read()
-        charbuffer.add(0, charCode)
-        if (charbuffer.size > 3) charbuffer.removeLast()
-        when (charbuffer){
-            listOf(68, 79, 27) -> println("LEFT")
-            listOf(65, 79, 27) -> println("UP")
-            listOf(66, 79, 27) -> println("DOWN")
-            listOf(67, 79, 27) -> println("RIGHT")
-        }
-    }
+    while (true) print(waitforkey(reader))
 }
