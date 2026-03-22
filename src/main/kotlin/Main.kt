3db038bf427560cb98d2d91dae013aa804c9d10c
discobeast <javanwyk@waimea.school.nz>
2026-03-23 11:01:25 +1300

    Fixed added utility functions for lists and beginnings of list delerations

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,5 +1,14 @@
 import org.jline.terminal.TerminalBuilder
 import org.jline.utils.NonBlockingReader
+import java.util.*
+
+fun <E> MutableList<E>.switch(index: Int, index2: Int) {
+    Collections.swap(this, index, index2)
+}
+
+fun <E> MutableList<E>.shift(index: Int, index2: Int) {
+    TODO("Shift target index value by X")
+}
 
 fun waitforkey(reader: NonBlockingReader): Int {
     var char: Int = -1
@@ -35,5 +44,11 @@ fun main() {
     val terminal = TerminalBuilder.builder().build()
     terminal.enterRawMode() // Disables line buffering
     val reader = terminal.reader()
-    while (true) print(waitforkey(reader))
+    val board = MutableList(12) { "#" }
+    board[0] = "&"
+    while (true) {
+        val keypress = waitforkey(reader)
+
+        print(board)
+    }
 }
