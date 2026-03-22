46c9f3ab3b9365a14bd6b75880928d96bbabcf4e
discobeast <96036990+discobeast@users.noreply.github.com>
2026-03-23 10:12:12 +1300

    Made check a wait function to make main wait for a valid key before continuing Should be fine as there are no background processes that would need a constantly looping main

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,20 +1,22 @@
 import org.jline.terminal.TerminalBuilder
-
-fun main() {
-    val terminal = TerminalBuilder.builder().build()
-    terminal.enterRawMode() // Disables line buffering
-    val reader = terminal.reader()
-    println("Press any key (Press 'q' to quit)...")
-    val charbuffer = MutableList(3) { 0 }
+fun waitforkey() {
     while (true) {
+        val terminal = TerminalBuilder.builder().build()
+        terminal.enterRawMode() // Disables line buffering
+        val reader = terminal.reader()
+        println("Press any key (Press 'q' to quit)...")
+        val charbuffer = MutableList(3) { 0 }
         val charCode = reader.read()
         charbuffer.add(0, charCode)
         if (charbuffer.size > 3) charbuffer.removeLast()
         when (charbuffer){
-            listOf(68, 79, 27) -> println("LEFT")
-            listOf(65, 79, 27) -> println("UP")
-            listOf(66, 79, 27) -> println("DOWN")
-            listOf(67, 79, 27) -> println("RIGHT")
+            listOf(65, 79, 27) -> 1
+            listOf(66, 79, 27) -> 2
+            listOf(68, 79, 27) -> 3
+            listOf(67, 79, 27) -> 4
         }
     }
+}
+fun main() {
+    waitforkey()
 }
